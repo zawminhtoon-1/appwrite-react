876cb800325f63c6f66ac4c95eb0c664c6fd458f
zawminhtoon-1 <mdcr.zawminhtoon@googlemail.com>
2026-03-14 03:19:38 +0900

    key

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,9 +1,9 @@
 import { Client, Account, Databases } from "node-appwrite";
 
 const client = new Client()
-    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
-    .setKey(import.meta.env.VITE_APPWRITE_API_KEY);
+    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key'] ?? '');
 
 const account = new Account(client);
 const databases = new Databases(client);
