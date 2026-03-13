39e87c977fbfc6b4e64149fcabb0315bd3139a66
zawminhtoon-1 <mdcr.zawminhtoon@googlemail.com>
2026-03-14 02:58:10 +0900

    add key

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,8 +1,9 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account, Databases } from "node-appwrite";
 
 const client = new Client()
     .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID)
+    .setKey(import.meta.env.VITE_APPWRITE_API_KEY);
 
 const account = new Account(client);
 const databases = new Databases(client);
